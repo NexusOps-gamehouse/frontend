361224db4df5efd1f36036d044945f3cdadfb754
DOOYEE0709 <somewhere.dooyee@gmail.com>
2026-07-28 16:00:28 +0900

    feat: 업로드 이미지 경로에 BACKEND_ORIGIN prefix 처리 (assetUrl)

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,10 @@
+import { assetUrl } from '../config';
+
 export default function Avatar({ user, size = '' }) {
   return (
     <div className={`av ${size}`}>
       {user?.profileImageUrl
-        ? <img src={user.profileImageUrl} alt={user.nickname} />
+        ? <img src={assetUrl(user.profileImageUrl)} alt={user.nickname} />
         : (user?.nickname?.[0] || '?')}
     </div>
   );
